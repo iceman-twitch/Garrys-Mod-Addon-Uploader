6a95257c9208f4d4f91635b949012f63f0f1191f
kem008 <iceman.twitch.contact@gmail.com>
2024-09-22 22:34:48 +0200

    Just small changes getting rid of old files

--- myaddon/gma_create.bat
+++
@@ -1,7 +0,0 @@
-cd /d "%~dp0"
-D:
-cd D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\
-
-gmad create -folder "%~dp0\" -out "%~dp0\myaddon.gma"
-
-pause
--- myaddon/publish_create.bat
+++
@@ -1,9 +0,0 @@
-cd /d "%~dp0"
-D:
-cd D:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\
-gmad create -folder "%~dp0\" -out "%~dp0\myaddon.gma"
-gmpublish create -addon "%~dp0\myaddon.gma" -icon "%~dp0\"
-
-pause
-cd /d "%~dp0"
-del myaddon.gma

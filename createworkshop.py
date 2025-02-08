8bf884560ed4164857148b3a0f31fbd1431f1575
iceman-twitch <iceman.twitch.contact@gmail.com>
2025-02-08 23:00:46 +0100

    python script better then bat
    it helps you to create the addon and upload it to the workshop

---
+++ createworkshop.py
@@ -0,0 +1,24 @@
+import subprocess
+import os
+
+
+# Paths
+gmad_path = r"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe"
+gmpublish_path = r"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"
+
+
+# Get the folder name from user input
+folder_name = input("Enter the name of your addon folder: ")
+
+addon_path = os.path.join(r"D:\Steam\GarrysMod\Garrys-Mod-Addon-Uploader", folder_name)  # Adjust the base path as needed
+output_gma_path = os.path.join(r"D:\Steam\GarrysMod\Garrys-Mod-Addon-Uploader", f"{folder_name}.gma")  # Output GMA file path
+pic_path = os.path.join(addon_path, "pic.jpg")  # pic.jpg file path
+
+# Create GMA file
+subprocess.run([gmad_path, "create", "-folder", addon_path, "-out", output_gma_path])
+
+
+# Upload to Workshop with thumbnail
+subprocess.run([gmpublish_path, "create", "-addon", output_gma_path, "-icon", pic_path])
+
+input("")
